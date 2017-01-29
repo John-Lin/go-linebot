796094281822be4236c00bab99b001ac8cdd5b69
John-Lin <linton.tw@gmail.com>
2017-01-30 01:44:34 +0800

    currency convert service

--- main.go
+++ main.go
@@ -2,14 +2,26 @@ package main
 
 import (
 	"log"
+	"strings"
 	"net/http"
 	"os"
 	"fmt"
 	"time"
+	"encoding/json"
+	"io/ioutil"
+	"strconv"
 
 	"github.com/line/line-bot-sdk-go/linebot"
 	"gopkg.in/gin-gonic/gin.v1"
 )
+
+type Response struct {
+  // The right side is the name of the JSON variable
+	Success       bool                 `json:"success"`
+	Timestamp     int64                `json:"timestamp"`
+	Source        string               `json:"source"`
+	Quotes        map[string]float64   `json:"quotes"`
+}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -56,7 +68,31 @@ func main() {
 			if event.Type == linebot.EventTypeMessage {
 				switch message := event.Message.(type) {
 				case *linebot.TextMessage:
-					_, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" OK!")).Do()
+					log.Printf("User ID is %v\n", event.Source.UserID)
+
+					if strings.HasPrefix(message.Text, "/") {
+						if IsLetter(message.Text[1:4]) {
+							r := convertCurrency()
+							if r.Success == true {
+								code := strings.ToUpper(message.Text[1:4])
+
+								if r.Quotes["USD" + code] == 0 {
+									_, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("沒有這個外匯代號")).Do()
+									if err != nil { log.Print(err) }
+								} else {
+									result := time.Unix(r.Timestamp, 0).String() + "\n" + FloatToString(r.Quotes["USD" + code])
+									_, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(result)).Do()
+									if err != nil { log.Print(err) }
+								}
+							} else {
+								_, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("Service Unreachable!")).Do()
+								if err != nil { log.Print(err) }
+								log.Printf("Service Unreachable!")
+							}
+
+						}
+					}
+					_, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do()
 					if err != nil { log.Print(err) }
 
 				}
@@ -65,4 +101,58 @@ func main() {
 	})
 
 	router.Run(":" + port)
+}
+
+func convertCurrency() *Response {
+	var (
+		queryString  string
+		apiKey       string
+		err          error
+		resp         Response
+		response     *http.Response
+		body         []byte
+	)
+
+	apiKey = os.Getenv("currencylayerAPIKey")
+
+	// Euro by default. Setting the base parameter in your request.
+	queryString = "?access_key=" + apiKey + "&source=USD&format=1"
+
+	// Use api.fixer.io to get a JSON response
+	response, err = http.Get("http://apilayer.net/api/live" + queryString)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer response.Body.Close()
+
+	// response.Body() is a reader type. We have
+	// to use ioutil.ReadAll() to read the data
+	// in to a byte slice(string)
+	body, err = ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Unmarshal the JSON byte slice to a GeoIP struct
+	err = json.Unmarshal(body, &resp)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Everything accessible in struct now
+	return &resp
+}
+
+func IsLetter(s string) bool {
+	for _, r := range s {
+		if (r < 'a' || r > 'z') && (r < 'A' || r > 'Z') {
+			return false
+		}
+	}
+	return true
+}
+
+func FloatToString(f float64) string {
+    // to convert a float number to a string
+    return strconv.FormatFloat(f, 'f', 3, 64)
 }
