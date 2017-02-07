9167fa1648230611dff0f730336a75ee2c5552ed
John-Lin <linton.tw@gmail.com>
2017-02-07 23:11:12 +0800

    go fmt main.go

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
-	"strings"
 	"net/http"
 	"os"
-	"fmt"
-	"time"
-	"encoding/json"
-	"io/ioutil"
 	"strconv"
+	"strings"
+	"time"
 
 	"github.com/line/line-bot-sdk-go/linebot"
 	"gopkg.in/gin-gonic/gin.v1"
@@ -54,41 +54,41 @@ func main() {
 
 		for _, event := range events {
 			switch event.Type {
-				case linebot.EventTypeMessage:
-					switch message := event.Message.(type) {
-						case *linebot.TextMessage:
-							log.Printf("User ID is %v\n", event.Source.UserID)
+			case linebot.EventTypeMessage:
+				switch message := event.Message.(type) {
+				case *linebot.TextMessage:
+					log.Printf("User ID is %v\n", event.Source.UserID)
 
-							if (strings.HasPrefix(message.Text, "/") && IsLetter(message.Text[1:4])) {
-								r := app.convertCurrency()
-								if r.Success == true {
-									code := strings.ToUpper(message.Text[1:4])
+					if strings.HasPrefix(message.Text, "/") && IsLetter(message.Text[1:4]) {
+						r := app.convertCurrency()
+						if r.Success == true {
+							code := strings.ToUpper(message.Text[1:4])
 
-									if r.Quotes["USD" + code] == 0 {
-										if err := app.replyText(event.ReplyToken, "沒有這個外匯代號"); err != nil {
-											log.Print(err)
-										}
-									} else {
-										// result := time.Unix(r.Timestamp, 0).String() + "\n" + FloatToString(r.Quotes["USD" + code])
-                    result := "USD/" + code + "  " + FloatToString(r.Quotes["USD" + code])
-										if err := app.replyText(event.ReplyToken, result); err != nil {
-											log.Print(err)
-										}
-									}
-
-								} else {
-									if err := app.replyText(event.ReplyToken, "Service Unreachable!"); err != nil {
-										log.Print(err)
-									}
-									log.Printf("Service Unreachable!")
+							if r.Quotes["USD"+code] == 0 {
+								if err := app.replyText(event.ReplyToken, "沒有這個外匯代號"); err != nil {
+									log.Print(err)
+								}
+							} else {
+								// result := time.Unix(r.Timestamp, 0).String() + "\n" + FloatToString(r.Quotes["USD" + code])
+								result := "USD/" + code + "  " + FloatToString(r.Quotes["USD"+code])
+								if err := app.replyText(event.ReplyToken, result); err != nil {
+									log.Print(err)
 								}
 							}
 
-						default:
-							log.Printf("Unknown message: %v", message)
+						} else {
+							if err := app.replyText(event.ReplyToken, "Service Unreachable!"); err != nil {
+								log.Print(err)
+							}
+							log.Printf("Service Unreachable!")
+						}
 					}
+
 				default:
-					log.Printf("Unknown event: %v", event)
+					log.Printf("Unknown message: %v", message)
+				}
+			default:
+				log.Printf("Unknown event: %v", event)
 			}
 
 		}
@@ -103,11 +103,11 @@ type CurrencyBot struct {
 }
 
 type Response struct {
-  // The right side is the name of the JSON variable
-	Success       bool                 `json:"success"`
-	Timestamp     int64                `json:"timestamp"`
-	Source        string               `json:"source"`
-	Quotes        map[string]float64   `json:"quotes"`
+	// The right side is the name of the JSON variable
+	Success   bool               `json:"success"`
+	Timestamp int64              `json:"timestamp"`
+	Source    string             `json:"source"`
+	Quotes    map[string]float64 `json:"quotes"`
 }
 
 // NewCurrencyBot function
@@ -125,15 +125,14 @@ func NewCurrencyBot(channelSecret, channelToken string) (*CurrencyBot, error) {
 	return &CurrencyBot{bot: bot}, nil
 }
 
-
 func (app *CurrencyBot) convertCurrency() *Response {
 	var (
-		queryString  string
-		apiKey       string
-		err          error
-		resp         Response
-		response     *http.Response
-		body         []byte
+		queryString string
+		apiKey      string
+		err         error
+		resp        Response
+		response    *http.Response
+		body        []byte
 	)
 
 	apiKey = os.Getenv("currencylayerAPIKey")
@@ -166,7 +165,6 @@ func (app *CurrencyBot) convertCurrency() *Response {
 	return &resp
 }
 
-
 func (app *CurrencyBot) replyText(replyToken, text string) error {
 	if _, err := app.bot.ReplyMessage(
 		replyToken,
@@ -187,6 +185,6 @@ func IsLetter(s string) bool {
 }
 
 func FloatToString(f float64) string {
-    // to convert a float number to a string
-    return strconv.FormatFloat(f, 'f', 3, 64)
+	// to convert a float number to a string
+	return strconv.FormatFloat(f, 'f', 3, 64)
 }
